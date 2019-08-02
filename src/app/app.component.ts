0d19338a713287402ebe9edb957b2bc5238a4082
nocuzat <nocuzat@gmail.com>
2019-08-02 12:36:37 +0200

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import * as jspdf from 'jspdf';  
+import html2canvas from 'html2canvas'; 
 
 @Component({
   selector: 'app-root',
@@ -10,6 +12,24 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+  public captureScreen()  
+  {  
+    var data = document.getElementById('contentToConvert');  
+    html2canvas(data).then(canvas => {  
+      // Few necessary setting options  
+      var imgWidth = 208;   
+      var pageHeight = 295;    
+      var imgHeight = canvas.height * imgWidth / canvas.width;  
+      var heightLeft = imgHeight;  
+  
+      const contentDataURL = canvas.toDataURL('image/png')  
+      let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF  
+      var position = 0;  
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)  
+      pdf.save('MYPdf.pdf'); // Generated PDF   
+    });  
+  }
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
--- src/app/page/recipt/recipt.page.ts
+++ src/app/page/recipt/recipt.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
+
+
+
 @Component({
   selector: 'app-recipt',
   templateUrl: './recipt.page.html',
@@ -15,6 +18,9 @@ tot:number;
 total:number;
 tots:number;
 totals
+
+
+
   constructor(private route: ActivatedRoute,private router: Router) {
     this.tot=0
     this.tots=0
@@ -22,6 +28,7 @@ totals
     this.pric=0
     this.total=0
     this.totals=0
+
    }
 
   ngOnInit() {
@@ -45,47 +52,62 @@ totals
 
 
 
-if(this.pack=="1 Packet"){
-  this.tots=5
-}else if(this.pack=="2 Packets"){
-  this.tots=10
-}else if(this.pack=="3 Packets"){
-  this.tots=15
-}else if(this.pack=="4 Packets"){
-  this.tots=20
-}else if(this.pack=="5 Packets"){
-  this.tots=25
-}
 
-
-
-if(this.extra=="Pancake"){
-  this.tot=20
-}else if(this.extra=="Muffin"){
-  this.tot=20
-}else if(this.extra=="Biscuits"){
-  this.tot=20
-}else if(this.extra=="Cake slice"){
-  this.tot=20
-}else if(this.extra=="Pizza slice"){
-  this.tot=20
-}
-
-this.totals = parseInt(this.pric)
-
-
-this.total= this.tot+ this.tots + this.totals
-
-console.log(this.tot)
-console.log(this.tots)
-console.log(this.pric)
-console.log(this.total)
 
 
 
   }
   onHome(){
     this.router.navigateByUrl("home")
+    this.tot=0
+    this.tots=0
+    this.total=0
   }
+
+print(){
+  window.print()
+}
+
+
+
+onTotal(){
+  if(this.pack=="1 Packet"){
+    this.tots=5
+  }else if(this.pack=="2 Packets"){
+    this.tots=10
+  }else if(this.pack=="3 Packets"){
+    this.tots=15
+  }else if(this.pack=="4 Packets"){
+    this.tots=20
+  }else if(this.pack=="5 Packets"){
+    this.tots=25
+  }
+  
+  
+  
+  if(this.extra=="Pancake"){
+    this.tot=20
+  }else if(this.extra=="Muffin"){
+    this.tot=20
+  }else if(this.extra=="Biscuits"){
+    this.tot=20
+  }else if(this.extra=="Cake slice"){
+    this.tot=20
+  }else if(this.extra=="Pizza slice"){
+    this.tot=20
+  }
+  
+  this.totals = parseInt(this.pric)
+  
+  
+  this.total= this.tot+ this.tots + this.totals
+  
+  console.log(this.tot)
+  console.log(this.tots)
+  console.log(this.pric)
+  console.log(this.total)
+}
+
+
 
 }
